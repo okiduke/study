1bf52cca67edaa4432d13bfdfe2cd9f53c161f55
okiduke <yasu.iphone@gmail.com>
2018-03-06 22:28:44 +0900

    復習:09-04開始〜09-05終了

--- 09/Template3.cpp
+++ 09/Template3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 template <typename TYPE> void FillArray(TYPE* array, size_t size, TYPE value) {
@@ -9,19 +10,14 @@ template <typename TYPE> void FillArray(TYPE* array, size_t size, TYPE value) {
 
 template <typename TYPE> void ShowArray(const TYPE* array, size_t size) {
     for(size_t i = 0; i < size; ++i) {
-        cout << array[i] << ' ';
+        cout << array[i] << endl;
     }
-    cout << endl;
 }
 
 int main() {
     const int SIZE = 3;
-    int i[SIZE];
-    double d[SIZE];
+    string str[SIZE];
 
-    FillArray(i, SIZE, 0);
-    FillArray<double>(d, SIZE, 0);
-
-    ShowArray(i, SIZE);
-    ShowArray(d, SIZE);
+    FillArray<string>(str, SIZE, "hoge");
+    ShowArray(str, SIZE);
 }
